8cff9a9492802ca834974042a2ad05286cbb8592
rockerle <16375280+rockerle@users.noreply.github.com>
2022-06-14 15:13:17 +0200

    Update to 1.19 (glitchy but works)

--- src/main/java/yan/lx/bedrockminer/utils/Messager.java
+++ src/main/java/yan/lx/bedrockminer/utils/Messager.java
@@ -2,33 +2,28 @@ package yan.lx.bedrockminer.utils;
 
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.MessageType;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-
-import java.util.UUID;
 
 public class Messager {
     public static void actionBar(String message){
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        minecraftClient.inGameHud.setOverlayMessage(new TranslatableText(message),false);
+        minecraftClient.inGameHud.setOverlayMessage(Text.translatable(message),false);
     }
     public static void rawactionBar(String message){
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        Text text = new LiteralText(message);
+        Text text = Text.literal(message);
         minecraftClient.inGameHud.setOverlayMessage(text,false);
     }
 
     public static void chat(String message){
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        minecraftClient.inGameHud.getChatHud().addMessage(new TranslatableText(message));
+        minecraftClient.inGameHud.getChatHud().addMessage(Text.translatable(message));
     }
 
     public static void rawchat(String message){
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        Text text = new LiteralText(message);
-        minecraftClient.inGameHud.addChatMessage(MessageType.SYSTEM,text, UUID.randomUUID());
+        Text text = Text.literal(message);
+        minecraftClient.inGameHud.getChatHud().addMessage(text);
     }
 }
 

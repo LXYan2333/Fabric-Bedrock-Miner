91c0e20d5b58b6d80fd452655c67f19684ec73cb
LXYan2333 <z00823823@126.com>
2026-06-26 10:49:11 +0800

    fix https://github.com/maruohon/malilib/issues/149 in our project

---
+++ src/client/java/com/github/lxyan2333/bedrockminer/client/mixin/BedrockMinerMixinPlugin.java
@@ -0,0 +1,49 @@
+package com.github.lxyan2333.bedrockminer.client.mixin;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class BedrockMinerMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.endsWith(".legacy.GuiTextFieldGenericMixin")) {
+            //? if <= 1.19.2 {
+            /*return true;
+            *///?} else {
+            return false;
+            //?}
+        }
+
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
---
+++ src/client/java/com/github/lxyan2333/bedrockminer/client/mixin/legacy/GuiTextFieldGenericMixin.java
@@ -0,0 +1,24 @@
+package com.github.lxyan2333.bedrockminer.client.mixin.legacy;
+
+import fi.dy.masa.malilib.gui.GuiTextFieldGeneric;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(GuiTextFieldGeneric.class)
+public abstract class GuiTextFieldGenericMixin extends EditBox {
+    public GuiTextFieldGenericMixin(Font font, int x, int y, int width, int height, Component message) {
+        super(font, x, y, width, height, message);
+    }
+
+    /**
+     * MaLiLib 0.12/0.13 calls EditBox#moveCursorTo here. In Mojang mappings that method
+     * virtual-dispatches back to setCursorPosition(), causing infinite recursion.
+     */
+    @Overwrite(remap = false)
+    public void setCursorPosition(int pos) {
+        super.setCursorPosition(pos);
+    }
+}
